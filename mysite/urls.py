2de420629a5bd7f87723eadbd4979af81ca7fe22
codecampNick <nick.jp.ross@gmail.com>
2019-04-07 13:23:59 -0600

    navbar work - showing active page - working

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='home'),
     path('admin/', admin.site.urls),
     url(r'^languages$', include('languages.urls')),
     url(r'projects/?', include('projects.urls'))
