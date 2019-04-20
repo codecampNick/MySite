0e42c0ab97f0a25bedd167467a25a57a8ebd8abc
codecampNick <nick.jp.ross@gmail.com>
2019-04-20 08:21:38 -0600

    Checking in work around employment. Added Aerios template and have it wired up. validated navigation works.

--- employment/urls.py
+++ employment/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='employment_home'),
-    url(r'dice-holdings-inc/?', views.diceholdings, name='aboutDiceHoldings'),
-    url(r'health-ecareers/?$', views.healthecareers, name='aboutHealthecareers'),
+    url(r'aerios-direct-inc', views.aeriosdirect, name='about_aeriosdirect'),
+    url(r'dice-holdings-inc/?', views.diceholdings, name='about_diceHoldings'),
+    url(r'health-ecareers/?$', views.healthecareers, name='about_healthecareers'),
 ]
--- employment/views.py
+++ employment/views.py
@@ -10,12 +10,23 @@ def index(request):
                    'employers': employers,
                    'accomplishments': accomplishments})
 
+def aeriosdirect(request):
+    return render(request, 'aerios-direct-inc.html',
+                  {
+                      'title': 'Aerios Direct Details',
+                  })
+
 
 def healthecareers(request):
     return render(request, 'health-ecareers.html',
-                  {'title': 'Health eCareers Details'})
+                  {
+                      'title': 'Health eCareers Details'
+                  })
 
 
 def diceholdings(request):
     return render(request, 'dice-holdings-inc.html',
-                  {'title': 'Dice Holdings Details'})
+                  {
+                      'title': 'Dice Holdings Details'
+                  })
+
